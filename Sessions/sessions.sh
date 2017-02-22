c1275f39325aaf868c0b7acb3ff717308a8f9550
Bobby Fincher <bobby.fincher@razorfish.com>
2017-02-22 08:11:56 -0500

    Remove expired Id and Password

--- Sessions/sessions.sh
+++ Sessions/sessions.sh
@@ -8,28 +8,28 @@ COUNTER=1
 
 while [  $COUNTER -gt 0 ]; do
         SRV1=0
-        SRV1=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-01-p:10190/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV1=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-01-p:10190/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         SRV2=0
-        SRV2=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-01-p:10290/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV2=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-01-p:10290/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         SRV3=0
-        SRV3=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-02-p:10190/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV3=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-02-p:10190/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         SRV4=0
-        SRV4=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-02-p:10290/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV4=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-02-p:10290/management/deployment/store.ear/subdeployment/store.war\?operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         SRV5=0
-        SRV5=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-03-p:10190/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV5=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-03-p:10190/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         SRV6=0
-        SRV6=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-03-p:10290/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV6=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-03-p:10290/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         SRV7=0
-        SRV7=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-04-p:10190/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV7=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-04-p:10190/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         SRV8=0
-        SRV8=`curl -m 1 -s -u bobby.fincher:V3r@Br@dl3y http://vbr-prd-dal-app-04-p:10290/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
+        SRV8=`curl -m 1 -s -u USERID:PASSWORD http://vbr-prd-dal-app-04-p:10290/management/deployment/store.ear/subdeployment/store.war?\operation\=resource\&include-runtime\&recursive\&json.pre
 tty\ | grep \"active-sessions | cut -d' ' -f7 | cut -d',' -f1`
         
         TS=`date +%F" "%T`
